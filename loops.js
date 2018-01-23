75961ada5d1b648881eb78037e109977fc135a50
Nic Travis <nptravis@yahoo.com>
2018-01-23 11:29:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,31 +1,29 @@
-var myArray = [];
-
 function forLoop(array) {
-  for (var i=0; i<=25; i++) {
-    if (i===1) {
-   array.push("I am ${i} strange loop${i === 0 ? '' : 's'}.");
- }
-    else {
-    array.push("I am ${i} strange loops.");
+  for (let i=0; i<25; i++) {
+    if(i === 1){
+      array.push(`I am ${i} strange loop.`);
+    }
+    else{
+      array.push(`I am ${i} strange loops.`);
+    }
   }
+  return array;
 }
-
-}
-return forLoop(array);
 
 function whileLoop(number) {
    while (number > 0) {
-   console.log('done');
+   console.log(number--);
 }
-};
+return "done";
+}
 
 function maybeTrue() {
-  return Math.random() >= 0.5
-};
+  return Math.random() >= 0.5;
+}
 
 function doWhile(array) {
    do {
-     delete array[0]
+     array.pop();
    } while (array.length > 0 && maybeTrue());
 return array;
-};
+}
